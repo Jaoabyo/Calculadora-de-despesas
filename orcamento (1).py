98fb2dd17b5b6d9e152d307022d96794a09c9381
João Vitor <gwawp88@gmail.com>
2026-09-11 13:33:09 -0300

    Enhance comments for expense calculations
    Added comments to clarify expense input and output.

--- orcamento (1).py
+++ orcamento (1).py
@@ -3,18 +3,18 @@
 renda = float(input("Quanto é sua Renda :"))
  
 #Despesas
-despesa_1 = input("Quanto foi sua despesa ?")
+despesa_1 = input("Quanto foi sua despesa ?") #Pergunta primeira despesa.
 numero_1 = float(despesa_1)
 
-despesa_2 = input("Quanto foi sua  segunda despesa ?")
+despesa_2 = input("Quanto foi sua  segunda despesa ?") #Pergunta segunda despesa.
 numero_2 = float(despesa_2)
 
-despesa_3 = input("Quanto foi sua terceira despesa despesa ?")
+despesa_3 = input("Quanto foi sua terceira despesa  ?") #Pergunta terceira despesa.
 numero_3 = float(despesa_3)
 
-total_despesas = numero_1+ numero_2 + numero_3 #Calculo das Despesas
+total_despesas = numero_1+ numero_2 + numero_3 #Calculo de todas as Despesas 
 renda_pos_despesas =   renda - total_despesas
 
-print(f"Renda: {renda:.2f}")
-print(f"Despesas: {total_despesas:.2f}")
-print(f"Saldo: {renda_pos_despesas:.2f}")
+print(f"Renda: {renda:.2f}") #Mostra a renda total
+print(f"Despesas: {total_despesas:.2f}") #Total de despesas
+print(f"Saldo: {renda_pos_despesas:.2f}") #Mostra o saldo após as despesas
